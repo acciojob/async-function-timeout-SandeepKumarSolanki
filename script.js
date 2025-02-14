aa5adbe8c024b7db91d7f1205372577c88835a42
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-15 01:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let input1 = document.getElementById('text');
+let input2 = document.getElementById('delay');
+let btn = document.getElementById('btn');
+
+let output = document.getElementById('output');
+
+let text1 = input1.innerText;
+let text2 = input2.innerText.val;
+
+
+new Promise((resolve , reject) =>{
+		if(text1.length == 0){
+			return div;
+		}else{
+			setTimeout(() => {
+				btn.addEventListener("click" , ()=>{
+					output.innerText = `Test - 1`
+				})
+			},text2)
+		}
+})
